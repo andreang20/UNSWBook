35e2f82687353982cfe4c6aaf0ed1bd281b26924
andreang20 <andreang20@gmail.com>
2017-09-27 21:00:51 +1000

    new schema

--- src/db_schema.sql
+++ src/db_schema.sql
@@ -1,28 +1,28 @@
 -- Run the following commands to delete all table and re-initialise database--
- drop table user_profile CASCADE;
- drop table wall_post CASCADE;
- drop table user_like CASCADE;
- drop table friend_list CASCADE;
- drop table logs CASCADE;
- drop table notification CASCADE;
+drop table user_profile CASCADE;
+drop table wall_post CASCADE;
+drop table user_like CASCADE;
+drop table friend_list CASCADE;
+drop table logs CASCADE;
+drop table notification CASCADE;
+drop table request CASCADE;
 
-
- -- user profile related sql --
- create table user_profile (
- 	username varchar(60) not null,
- 	password varchar(20) not null,
- 	first_name varchar(60) not null,
- 	last_name varchar(60) not null,
- 	email varchar(200) not null,
- 	gender varchar(20) not null,
- 	date_of_birth date not null,
- 	session_id int,
- 	is_banned boolean not null,
- 	primary key(username),
- 	constraint chk_gender check
- 	(gender = 'male' or gender = 'female')
- 	-- session_id null == user is not online --
- );
+-- user profile related sql --
+create table user_profile (
+	username varchar(60) not null,
+	password varchar(20) not null,
+	first_name varchar(60) not null,
+	last_name varchar(60) not null,
+	email varchar(200) not null,
+	gender varchar(20) not null,
+	date_of_birth date not null,
+	session_id int,
+	is_banned boolean not null,
+	primary key(username),
+	constraint chk_gender check
+	(gender = 'Male' or gender = 'Female')
+	-- session_id null == user is not online --
+);
 -- end of profile sql--
 
 --wall post related sql --
@@ -51,31 +51,41 @@ create table user_like(
 create table friend_list(
 	username_primary varchar(60) not null,
 	username_secondary varchar(60) not null,
-	accepted boolean,
 	foreign key (username_primary) references user_profile(username),
 	foreign key (username_secondary) references user_profile(username),
-	primary key (username_primary, username_secondary)--,
-	--constraint chk_boolean_accepted check
-	--(accepted = 0 or accepted = 1)
-	-- if rejected delete the entry from database, accepted == false means the other party not accept yet --
+	primary key (username_primary, username_secondary)
 );
 
 -- user logs
 create table log(
-  log_id serial,
-  username varchar(60) not null,
-  user_action varchar(1000) not null,
-  time timestamp not null,
-  PRIMARY KEY (log_id),
-  FOREIGN KEY (username) REFERENCES  user_profile(username)
+	log_id serial,
+	username varchar(60) not null,
+	user_action varchar(1000) not null,
+	time timestamp not null,
+	PRIMARY KEY (log_id),
+	FOREIGN KEY (username) REFERENCES  user_profile(username)
 );
 
 
 create table notification(
-  notification_id serial,
-  username varchar(60) not null,
-  user_action varchar(1000) not null,
-  time timestamp not null,
-  PRIMARY KEY (notification_id),
-  FOREIGN KEY (username) REFERENCES  user_profile(username)
+	notification_id serial,
+	username varchar(60) not null,
+	user_action varchar(1000) not null,
+	time timestamp not null,
+	PRIMARY KEY (notification_id),
+	FOREIGN KEY (username) REFERENCES  user_profile(username)
+);
+
+
+-- friend request email --
+create table request(
+	id serial,
+	sender varchar(60) not null,
+	receiver varchar(60) not null,
+	accepted boolean not null
+	primary key (id),
+	foreign key (sender) references user_profile(username),
+	foreign key (receiver) references user_profile(username)
+	-- accepted false == not yet confirm, true == confirm--
+	-- confirm record it to friend_list and send notification--
 );
