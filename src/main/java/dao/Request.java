5d314b6f636f02ac54eea9bd7eb65326732817d9
Clinton <clintonfeng@gmail.com>
2017-09-29 18:51:23 +1000

    changed request table because we only need 1 request for sender ->receiver

--- src/main/java/dao/Request.java
+++ src/main/java/dao/Request.java
@@ -1,28 +1,16 @@
 package dao;
 
 public class Request {
-    public static final int INVALID = -1;
-
-    private int id;
     private String sender;
     private String receiver;
     private boolean accepted;
 
-    public Request(int id, String sender, String receiver, boolean accepted) {
-        this.id = id;
+    public Request(String sender, String receiver, boolean accepted) {
         this.sender = sender;
         this.receiver = receiver;
         this.accepted = accepted;
     }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
+    
     public String getSender() {
         return sender;
     }
